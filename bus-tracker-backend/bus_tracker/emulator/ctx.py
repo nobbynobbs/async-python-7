64b1d8a377dfdecf28157ea7924330f05bb458d6
Roman Bolkhovitin <rbolkhovitin@gmail.com>
2019-12-29 17:02:46 +0300

    fix typing

--- bus-tracker-backend/bus_tracker/emulator/ctx.py
+++ bus-tracker-backend/bus_tracker/emulator/ctx.py
@@ -1,5 +1,5 @@
-import contextvars
+from contextvars import ContextVar
 
 # It's absolutely unnecessary to use, I'd just been curios how it works,
 # so I use ContextVar for access to worker_id in reconnect decorator
-worker_index = contextvars.ContextVar("worker_index")
+worker_index: ContextVar[int] = ContextVar("worker_index")
